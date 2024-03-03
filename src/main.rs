906a984b6e17e289c56d0795db1eb4b98554c1cb
pedro <pedro.jcaceresl@gmail.com>
2024-03-03 10:57:50 -0300

    first commit: Guess program iwth Rust

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use rand::prelude::*;
+use std::{cmp::Ordering, io};
+
+fn main() {
+    let correct = rand::thread_rng().gen_range(1..=10);
+    println!("Hey, guess a number 1-10:");
+
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error reading input");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(e) => {
+                println!("Error with parse, try again {e}");
+                continue;
+            }
+        };
+
+        match guess.cmp(&correct) {
+            Ordering::Greater => println!("You guessed too high."),
+            Ordering::Less => println!("You guessed too low"),
+            Ordering::Equal => {
+                println!("You guessed CORRECT");
+                break;
+            }
+        };
+    }
+}
